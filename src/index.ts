895630eb342f5c9173ba21ea6c4e3dbbe5892910
vraj <vrajpatel00222@gmail.com>
2025-12-12 15:19:10 -0800

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return Response.json({
+			message: "you did not send a get request"
+		});
 	},
-} satisfies ExportedHandler<Env>;
+};
